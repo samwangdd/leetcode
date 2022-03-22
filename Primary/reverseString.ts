317ba396791b7019705b36bbc9c2833ba89a237b
王森 <wyysammore@gmail.com>
2022-03-22 22:59:28 +0800

    feat: reverse string

---
+++ Primary/reverseString.ts
@@ -0,0 +1,30 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function (s) {
+  const length = s.length
+  for (let i = 0, j = length - 1; i < j; ++i, --j) {
+    ;[s[i], s[j]] = [s[j], s[i]]
+  }
+  return s
+}
+
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString2 = function (s) {
+  let i = 0
+  let j = s.length - 1
+
+  while (i < j) {
+    const temp = s[i]
+    s[i] = s[j]
+    s[i] = temp
+    i++
+    j--
+  }
+
+  return s
+}
