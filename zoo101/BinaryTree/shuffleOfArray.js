84bb9a4d58bd953752dbaf47470c208e6ade51d1
Sam <wyysammore@gmail.com>
2022-06-07 09:47:16 +0800

    feat(bst): shuffle of array

--- zoo101/BinaryTree/shuffleOfArray.js
+++ zoo101/BinaryTree/shuffleOfArray.js
@@ -28,11 +28,21 @@ Solution.prototype.shuffle = function () {
   const nums = this.nums.slice(0)
   const len = nums.length
   for (let i = 0; i < len; i += 1) {
-    const random = Math.floor(Math.random() * nums.length)
+    const random = Math.floor(Math.random() * len)
     arr[i] = nums[random]
     nums.splice(random, 1)
   }
   return arr
+}
+
+Solution.prototype.shuffleFast = function () {
+  const nums = this.nums.slice(0)
+  const len = nums.length
+  for (let i = len - 1; i > 0; i -= 1) {
+    const targetIndex = Math.floor(Math.random() * (i + 1))
+    ;[nums[i], nums[targetIndex]] = [nums[targetIndex], nums[i]]
+  }
+  return nums
 }
 
 /**
