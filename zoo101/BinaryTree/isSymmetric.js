ed391d59c5cc5de95ae9e30c323d8cb7fba65881
Sam <wyysammore@gmail.com>
2022-06-10 10:09:47 +0800

    feat(zoo101): isSymmetric

---
+++ zoo101/BinaryTree/isSymmetric.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+
+/**
+ * 递归实现
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function (root) {
+  // n 和 n-1 的关系是什么？
+  if (!root) {
+    return true
+  }
+  return isSameTree(root.left, root.right)
+}
+
+function isSameTree(rl, rr) {
+  if (rl === null) return rr === null
+  if (rr === null) return rl === null
+
+  if (rl !== rr) {
+    return false
+  }
+
+  // 子节点很多如何解决它们之间的关系？
+  return isSameTree(rl.left, rr.right) && isSameTree(rl.right, rr.left)
+}
