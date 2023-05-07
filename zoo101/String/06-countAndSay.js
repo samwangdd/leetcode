d89e02ddc38d87c650fc9317cb6c4f76bca8fbb6
samwangdd <wyysammore@gmail.com>
2023-05-07 10:37:34 +0800

    feat(101): countAndSay

--- zoo101/String/06-countAndSay.js
+++ zoo101/String/06-countAndSay.js
@@ -1,18 +1,10 @@
 function countAndSay(n) {
-    console.log('n', n)
-    if (n === 1) {
-        return '1'
+    let res = '1';
+    for (let i = 1; i < n; i++) {
+        res = res.replace(/(\d)\1*/g, item => {
+            return `${item.length}${item[0]}`
+        })
     }
-    const preCount = countAndSay(n - 1);
-    console.log('a', preCount);
-    /**
-     * 这个正则表达式将匹配连续出现的数字并将其捕获到一个分组中
-     * (\d) 表示捕获数字
-     * \1 匹配前面括号内匹配到的内容
-     * (\d)\1* 匹配相邻数字、且数字相同的内容
-     */
-    return preCount.replace(/(\d)\1*/g, item => {
-        console.log('b', item)
-        return `${item.length}${item[0]}`
-    })
+
+    return res
 }
