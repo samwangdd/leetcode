2a17544d84ec3d6e3d3ad2b2d5bc6354828c125b
samwangdd <wyysammore@gmail.com>
2023-05-23 22:53:47 +0800

    feat(101): plusOne

--- zoo101/Array/05-plusOne.js
+++ zoo101/Array/05-plusOne.js
@@ -1,3 +1,17 @@
-function plusOne(digits) {
-  return (BigInt(digits.join("")) + 1n).toString().split("");
-}
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function (digits) {
+    let num = 0;
+    const len = digits.length;
+    for (let i = 0; i < len; i++) {
+        const v = digits[i];
+        num = BigInt(num) * 10n + BigInt(v);
+        if (i === len - 1) {
+            num = BigInt(num) + 1n;
+        }
+    }
+
+    return num.toString().split("");
+};
