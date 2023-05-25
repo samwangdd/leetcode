de20a978467620bfe4e5e3c226ab9bc2914e34ba
samwangdd <wyysammore@gmail.com>
2023-05-25 22:28:13 +0800

    feat(101): moveZero

--- zoo101/Array/07-moveZero.js
+++ zoo101/Array/07-moveZero.js
@@ -1,12 +1,15 @@
 function moveZero(nums) {
-    const lens = nums.length;
-    for (let i = 0; i < lens; i++) {
-        if (nums[i] === 0) {
-            nums.splice(i, 1)
-            nums.push(0)
-            len --;
-        }
+  // 双指针
+  let j = 0; // j 用来记录非零的数
+  
+  for (let i = 0; i < nums.length; i++) {
+  
+    // i 不等于0，则赋值给 j
+    if (nums[i] !== 0) {
+      nums[j] = nums[i];
+      j++;
     }
+  }
 
-    return nums;
+  return nums.fill(0, j, nums.length);
 }
