1a5fd4d43c987b070792c6faf0197a682c70f0d8
samwangdd <wyysammore@gmail.com>
2023-05-25 22:38:51 +0800

    fix(101): intersect

--- zoo101/Array/08-intersect.js
+++ zoo101/Array/08-intersect.js
@@ -1,20 +1,20 @@
 function intersect(nums1, nums2) {
-  const hashObject = {};
-  for (let i = 0; i < nums1.length; i++) {
-    if (hashObject[nums1[i]]) {
-      hashObject[nums1[i]] += 1;
-    } else {
-      hashObject[nums1[i]] = 1;
+    const hashObject = {};
+    for (let i = 0; i < nums1.length; i++) {
+      if (hashObject[nums1[i]]) {
+        hashObject[nums1[i]] += 1;
+      } else {
+        hashObject[nums1[i]] = 1;
+      }
     }
-  }
-
-  const result = [];
-  for (let j = 0; j < nums2.length; j++) {
-    if (hashObject[nums2[j]]) {
-      result.push(hashObject[nums2[i]]);
-      hashObject[nums2[i]] -= 1;
+  
+    const result = [];
+    for (let j = 0; j < nums2.length; j++) {
+      if (hashObject[nums2[j]]) {
+        result.push(nums2[j]);
+        hashObject[nums2[j]] -= 1;
+      }
     }
+  
+    return result;
   }
-
-  return result;
-}
